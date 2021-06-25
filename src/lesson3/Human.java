4b98d8858f2bdfd2292ab574493037abbf18a829
Babiyv <377064910Vo>
2021-06-25 06:34:53 +0300

    Lesson 3 classwork

---
+++ src/lesson3/Human.java
@@ -0,0 +1,7 @@
+package lesson3;
+
+public class Human<T,U, Y> {
+    private T first;
+    private U second;
+    private Y third;
+}
---
+++ src/lesson3/Main.java
@@ -0,0 +1,9 @@
+package lesson3;
+
+import lesson1.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+        Human<String, Integer, Animal> human = new Human<String, Integer, Animal>();
+    }
+}
