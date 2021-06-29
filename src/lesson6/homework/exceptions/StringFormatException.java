b07349b7a980dc38b55970b6f9dc14adcbce43ce
Babiyv <377064910Vo>
2021-06-29 15:25:30 +0300

    Lesson 6 homework is done

---
+++ src/lesson6/homework/exceptions/StringFormatException.java
@@ -0,0 +1,8 @@
+package lesson6.homework.exceptions;
+
+/*2. В папке exceptions создаем свою проверяемую ошибку StringFormatException;*/
+public class StringFormatException extends Exception {
+    public StringFormatException(String s) {
+        super(s);
+    }
+}
