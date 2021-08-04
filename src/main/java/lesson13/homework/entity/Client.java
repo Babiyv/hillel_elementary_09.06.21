8d36fb468d52c18f365f52501b077881bc16479b
Babiyv <377064910Vo>
2021-08-04 11:42:03 +0300

    homework Lesson 13: created entity Client

---
+++ src/main/java/lesson13/homework/entity/Client.java
@@ -0,0 +1,12 @@
+package lesson13.homework.entity;
+
+import lombok.Data;
+
+@Data // - анотация "создающая под капотом" геттеры, сеттеры и ту стринг (без неодбходимости прописывать вручную). Часть подключенной библиотеки lombok (внутри pom.xml).
+public class Client {
+    private int id;
+    private String name;
+    private String email;
+    private int phone;
+    private String about;
+}
