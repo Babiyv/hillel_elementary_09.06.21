66ec37a0b156b00cb308a884758bd01daf83d0b3
Babiyv <377064910Vo>
2021-08-04 11:02:23 +0300

    homework Lesson 13: created entity Account with lombok

---
+++ src/main/java/lesson13/homework/entity/Account.java
@@ -0,0 +1,12 @@
+package lesson13.homework.entity;
+
+import lombok.Data;
+
+/*3. Создать сущности для этих daos, опираясь на поля таблиц из ДЗ_11 по работе с SQL; */
+@Data
+public class Account {
+    private int id;
+    private int client_id;
+    private String number;
+    private double value;
+}
