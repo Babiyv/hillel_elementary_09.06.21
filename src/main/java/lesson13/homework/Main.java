635043a1c2a91276947efe507fc6fcadf6179672
Babiyv <377064910Vo>
2021-08-04 17:40:33 +0300

    created class Main for methods testing

---
+++ src/main/java/lesson13/homework/Main.java
@@ -0,0 +1,10 @@
+package lesson13.homework;
+
+import lesson13.homework.dao.ClientDao;
+
+
+/*В задании не требовалось, но создал просто для проверки методов*/
+public class Main {
+    public static void main(String[] args) {
+    }
+}
