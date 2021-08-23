99f83924c74ee2f0b4043c4d83d545e61a19dcee
Babiyv <377064910Vo>
2021-08-23 22:30:40 +0300

    formated lines for better visualisation

--- src/main/java/lesson19/homework/thread/ExtendsThread.java
+++ src/main/java/lesson19/homework/thread/ExtendsThread.java
@@ -1,10 +1,11 @@
 package lesson19.homework.thread;
+
 import lesson19.homework.utils.ExtraClass;
 
 //        1. Создайте 3 класс для работы в многопоточном режиме. Реализовать классы для работы с потоками тремя разными способами:
 //        3. В методах потоков задать свои имена для идентификации и написать логику циклического прохождения (1000 итераций)
 //      и вызова дополнительного метода описанного ранее;
-public class ExtendsThread extends Thread{
+public class ExtendsThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 1000; i++) {
--- src/main/java/lesson19/homework/thread/ImplementsRunnable.java
+++ src/main/java/lesson19/homework/thread/ImplementsRunnable.java
@@ -5,7 +5,7 @@ import lesson19.homework.utils.ExtraClass;
 //        1. Создайте 3 класс для работы в многопоточном режиме. Реализовать классы для работы с потоками тремя разными способами:
 //        3. В методах потоков задать свои имена для идентификации и написать логику циклического прохождения (1000 итераций)
 //      и вызова дополнительного метода описанного ранее;
-public class ImplementsRunnable implements Runnable{
+public class ImplementsRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 1000; i++) {
