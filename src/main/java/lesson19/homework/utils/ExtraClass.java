163234a07557d2fe0d6c5bd962552cec1f3dc279
Babiyv <377064910Vo>
2021-08-23 22:04:13 +0300

    was try a different variation for threads synchronization

--- src/main/java/lesson19/homework/utils/ExtraClass.java
+++ src/main/java/lesson19/homework/utils/ExtraClass.java
@@ -7,11 +7,44 @@ public class ExtraClass {
     static int counter = 0;
 
     public synchronized static int readThreadNameReturnIncrementedVariable() {
-        // Считываем и выводим на консоль имя потока в котором выполняется метод:
-        System.out.println("Running by thread named: ".concat(Thread.currentThread().getName()));
-        // выводим на консоль переменную этого класса:
-        System.out.println(counter);
-        // инкрементируем переменную и отправляем результат вызывающей стороне
-        return ++counter;
+            // Считываем и выводим на консоль имя потока в котором выполняется метод:
+            System.out.println("Running by thread named: ".concat(Thread.currentThread().getName()));
+            // выводим на консоль переменную этого класса:
+            System.out.println(counter);
+            // инкрементируем переменную и отправляем результат вызывающей стороне
+            return ++counter;
     }
+
+
+    // попробовал второй вариант с синхронизацией по объекту (но не подпадает под условие, что должна быть только одна переменная,
+/*    static final String objectForSynchronization = "lock";
+    static final ExtraClass objectForSynchronization1 = new ExtraClass();
+    static int counter = 0;
+
+    public static int readThreadNameReturnIncrementedVariable() {
+        synchronized (objectForSynchronization) {
+            // Считываем и выводим на консоль имя потока в котором выполняется метод:
+            System.out.println("Running by thread named: ".concat(Thread.currentThread().getName()));
+            // выводим на консоль переменную этого класса:
+            System.out.println(counter);
+            // инкрементируем переменную и отправляем результат вызывающей стороне
+            return ++counter;
+        }
+    }*/
+
+    // третий вариант:
+/*    public static int readThreadNameReturnIncrementedVariable() {
+        synchronized (String.valueOf(counter)) {
+            // Считываем и выводим на консоль имя потока в котором выполняется метод:
+            System.out.println("Running by thread named: ".concat(Thread.currentThread().getName()));
+            // выводим на консоль переменную этого класса:
+            System.out.println(counter);
+            // инкрементируем переменную и отправляем результат вызывающей стороне
+            return ++counter;
+        }
+    }*/
+
+
+
+
 }
